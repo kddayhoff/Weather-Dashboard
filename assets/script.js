b0c66ec76f641bc8b992af844bd6a450ca3ba5a6
Karla Dayhoff <kddayhoff@gmail.com>
2020-03-19 16:33:21 -0500

    got items logging to local storage,  unable to get them yet

--- assets/script.js
+++ assets/script.js
@@ -1,16 +1,23 @@
 $(document).ready(function() {
 
 
-    $("#user-input").keyup(function( event ) {
-        if ( event.keyCode === 13 ) {
-         event.preventDefault();
+    // $("#user-input").keyup(function( event ) {
+    //     if ( event.keyCode === 13 ) {
+    //      event.preventDefault();
         
 $("#searchBtn").on("click", function(event) {
     event.preventDefault;
+
 var userInput = $("#user-input").val();
+            console.log(userInput);
+            localStorage.setItem("City:", userInput);
+var getUserInput = localStorage.getItem("City:");
+$("#city-list").append(getUserInput);
+
 //Userinput field is NOT working at the moment
 var apiKey = "&appid=b74f70f75df1dd598f40a1fa0a327642";
 var queryURL = "https://api.openweathermap.org/data/2.5/weather?q=" + userInput + apiKey;
+
 
     $.ajax({
         url: queryURL,
@@ -21,19 +28,14 @@ var queryURL = "https://api.openweathermap.org/data/2.5/weather?q=" + userInput 
         console.log(queryURL);
 
         var name = response.name;
-       
 
         var temp = (response.main.temp);
        
-
-        var tempF = (response.main.temp - 273.15) * 1.80 + 32;
-        
+        var tempF = (response.main.temp - 273.15) * 1.80 + 32; 
 
         var humidity = response.main.humidity
         
-
         var windSpeed = response.wind.speed;
-       
 
         var lng = response.coord.lon;
         console.log(lng);
@@ -41,10 +43,8 @@ var queryURL = "https://api.openweathermap.org/data/2.5/weather?q=" + userInput 
         var lat = response.coord.lat;
         console.log(lat);
         
-      
         var uvIndex = "http://api.openweathermap.org/data/2.5/uvi?&lat=" + lat + "&lon=" + lng + apiKey;
 
-    
         $.ajax({
             url: uvIndex,
             method: 'GET',
@@ -54,13 +54,7 @@ var queryURL = "https://api.openweathermap.org/data/2.5/weather?q=" + userInput 
             var date = request.date_iso;
             
             var uvIndex = request.value;
-           
-
-
-        // var weatherData = $("<div>");
-        // weatherData.addClass("data-item");
-        // weatherData.add("li");
-        // $("#weather-info").append(weatherData);
+     
 
         $("#weather-info").append("<li>" + "City: " + name + "</li>");
         $("#weather-info").append("<li>" + "Current Date:" + date + "</li>");
@@ -68,49 +62,64 @@ var queryURL = "https://api.openweathermap.org/data/2.5/weather?q=" + userInput 
         $("#weather-info").append("<li>" + "Temperature Kelvin: " + temp + "</li>");
         $("#weather-info").append("<li>" + "Humidity: " + humidity + "</li>");
         $("#weather-info").append("<li>" + "Wind Speed: " + windSpeed + "</li>");
-        $("#weather-info").append("<li>" + "UV Index" + uvIndex + "</li>");
+        $("#weather-info").append("<li>" + "UV Index " + uvIndex + "</li>");
 
-       
+        
 
         var fiveDayForecast = "https://api.openweathermap.org/data/2.5/forecast?lat=" + lat + "&lon=" + lng + apiKey;
 
+
+
+        
+//=============================================================
+//Five day forecast here
         $.ajax({
             url: fiveDayForecast,
             method: 'GET',
 
         }).then(function(forecast) {
-            // console.log(forecast);
+            console.log(forecast);
             
             //console.log("temp: " + forecast.list[0].main.temp)
             // console.log("temp min: " + forecast.list[0].main.temp_min)
             // console.log("temp max: " + forecast.list[0].main.temp_max)
-           
+         
             for (var i = 0; i < forecast.list.length; i++) {
-                console.log(forecast.list[i].main.temp)
-                console.log(forecast.list[i].main.temp_min)
-                console.log(forecast.list[i].main.temp_max)
+                var timeOfDay = forecast.list[i].dt_txt.substring(10);
+                
+                // console.log(timeOfDay);
+                // console.log("Current temp:" + forecast.list[i].main.temp)
+                // console.log("Min temp" + forecast.list[i].main.temp_min)
+                // console.log("Max temp" + forecast.list[i].main.temp_max)
+                // var minTemp = forecast.list[i].main.temp.temp_min;
+                // //var maxTemp = forecast.list[i].main.temp.temp_max;
+
+                // var forecastMin = minTemp.map(function (temp) {
+                //     return forecastMin.list[i].main.temp_min === "Minimum Temp";
+                    
+               
+                // }); 
+                // console.log(forecastMin);
+                // // var forecastMax = forecast.filter(function (temp) {
+                // //     return temp.list[i].main.temp_max === "Maximum Temp";
+                // // } )
+                
+                // //console.log(forecastMax);
+                // //var midday = (forecast.dates.indexOf("12:00:00") > -1);
+                // // if (timeOfDay === "12:00:00") {
+                // //     ;
+                // // }
+                // //console.log(midday);
                 if (i === 5) {
                     break;
                 } 
-            }
-
-        
-
-
-           $("#weather-info").append("<div>" + fiveDayForecast + "</div>)");
-
-        })
-        //console.log(forecast);
-    
-        
-
-
-        })
-     });
-  });
-}
-})
-})
+               }
+             })
+           })
+         });
+       });
+      
+   })
 
  
  
