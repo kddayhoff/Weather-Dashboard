f0453947018f2138b367354c02b1d59adaec27c4
Karla Dayhoff <kddayhoff@gmail.com>
2020-03-19 20:59:15 -0500

    five day forecast working in console, local storage keeping values and printing on page

--- assets/script.js
+++ assets/script.js
@@ -1,18 +1,50 @@
+function displayCities () {
+    
+   $("#cities").empty();
+    
+    for ( var i = 0; i < cities.length; i++) {
+        var newItem = $("<li>").text(cities[i]);
+        
+        $("#cities").append(newItem);
+    }
+
+}
+
+if (localStorage.cities) {
+    var cities = JSON.parse(localStorage.getItem("cities"));
+    }
+    else {
+        var cities = []
+    }
+
 $(document).ready(function() {
 
 
-    // $("#user-input").keyup(function( event ) {
-    //     if ( event.keyCode === 13 ) {
-    //      event.preventDefault();
+displayCities();
+console.log(cities);
+
+    
+    
+//  $(document).on("keypress", function(x){                   if(x.which == 13) {
+//             input = $("#user-input").val();
+//         }
+//     })
         
 $("#searchBtn").on("click", function(event) {
     event.preventDefault;
 
 var userInput = $("#user-input").val();
-            console.log(userInput);
-            localStorage.setItem("City:", userInput);
+cities.push(userInput);
+localStorage.setItem("cities", JSON.stringify(cities));
+console.log(cities)
+console.log(userInput);
+displayCities();
+        //call function for cities
 var getUserInput = localStorage.getItem("City:");
 $("#city-list").append(getUserInput);
+
+//clear user input here
+
 
 //Userinput field is NOT working at the moment
 var apiKey = "&appid=b74f70f75df1dd598f40a1fa0a327642";
@@ -73,46 +105,23 @@ var queryURL = "https://api.openweathermap.org/data/2.5/weather?q=" + userInput 
         
 //=============================================================
 //Five day forecast here
+
+
+
         $.ajax({
             url: fiveDayForecast,
             method: 'GET',
 
         }).then(function(forecast) {
             console.log(forecast);
-            
-            //console.log("temp: " + forecast.list[0].main.temp)
-            // console.log("temp min: " + forecast.list[0].main.temp_min)
-            // console.log("temp max: " + forecast.list[0].main.temp_max)
+           
          
             for (var i = 0; i < forecast.list.length; i++) {
-                var timeOfDay = forecast.list[i].dt_txt.substring(10);
-                
-                // console.log(timeOfDay);
-                // console.log("Current temp:" + forecast.list[i].main.temp)
-                // console.log("Min temp" + forecast.list[i].main.temp_min)
-                // console.log("Max temp" + forecast.list[i].main.temp_max)
-                // var minTemp = forecast.list[i].main.temp.temp_min;
-                // //var maxTemp = forecast.list[i].main.temp.temp_max;
+                if(forecast.list[i].dt_txt.indexOf("15:00:00")!== -1) {
+                    console.log(forecast.list[i]);
+                }
 
-                // var forecastMin = minTemp.map(function (temp) {
-                //     return forecastMin.list[i].main.temp_min === "Minimum Temp";
-                    
-               
-                // }); 
-                // console.log(forecastMin);
-                // // var forecastMax = forecast.filter(function (temp) {
-                // //     return temp.list[i].main.temp_max === "Maximum Temp";
-                // // } )
                 
-                // //console.log(forecastMax);
-                // //var midday = (forecast.dates.indexOf("12:00:00") > -1);
-                // // if (timeOfDay === "12:00:00") {
-                // //     ;
-                // // }
-                // //console.log(midday);
-                if (i === 5) {
-                    break;
-                } 
                }
              })
            })
